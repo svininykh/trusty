38524da3fb3448ae43c9b295ede102b23c9f7e9d
Vladimir Petrukhin <man4j@ya.ru>
2015-08-17 10:51:14 +0300

    change example test

--- src/test/java/ru/ussgroup/security/trusty/VerifySignatureExampleTest.java
+++ src/test/java/ru/ussgroup/security/trusty/VerifySignatureExampleTest.java
@@ -37,7 +37,9 @@ public class VerifySignatureExampleTest {
         
         TrustyCachedOCSPValidator cachedOCSPValidator = new TrustyCachedOCSPValidator(kalkanOCSPValidator, 5, 60);
         
-        TrustyCertificateValidator validator = new TrustyCertificateValidator.Builder(cachedOCSPValidator).build();
+        TrustyCertificateValidator validator = new TrustyCertificateValidator.Builder(cachedOCSPValidator).checkIsEnterprise()
+                                                                                                          .checkForSigning()
+                                                                                                          .build();
         
         validator.validate(cert);
     }
